85076a5b8c7c768e16b41fc95de6c9c52e23e0b5
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-08-22 10:46:28 -0700

    add docs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  cloudName: 'picturecloud7',
+  uploadPreset: 'ng_pwa',
+  firebase: {
+    apiKey: "AIzaSyCOHbFDmmaYYbMjxFstuDu1sth1VgRu9DM",
+    authDomain: "ng-pwa-d812e.firebaseapp.com",
+    databaseURL: "https://ng-pwa-d812e.firebaseio.com",
+    projectId: "ng-pwa-d812e",
+    storageBucket: "",
+    messagingSenderId: "1065365724703"
+  }
+};
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/selfiemator'),
+      dir: require('path').join(__dirname, '../coverage/ng-pwa'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
